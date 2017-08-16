60ad718fa4e48721058a0526fd19da773fb18066
zarkin200 <zarkin200@GACDOL-2MWH1G2>
2017-08-16 09:45:29 -0400

    start routing

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,20 +3,30 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: 
-    `<div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+    `<div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
         </div>
         
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
+        </div>
     </div>
     `,
     styles: [`
+        
+        .thumbnail {min-height: 210px;}
         .pad-left {margin-left: 10px; }
         .well div { color: #bbb; }
     `]
@@ -27,6 +37,10 @@ export class EventThumbnailComponent{
     logFoo() {
         console.log('foo')
     }
-
+    getStartTimeStyle() {
+        if(this.event && this.event.time === '8:00 am') 
+            return {color: '#003300', 'font-weight': 'bold'}
+        return {}
+    }
     
 }
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,30 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service'
+import { ToastrService } from '../common/toastr.service'
 
 @Component({
-    selector: 'events-list',
+    
     template: `
     <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr />
-        <event-thumbnail [event]="event1"></event-thumbnail>
+        <div class="row">
+            <div class="col-md-5" *ngFor="let event of events">
+                <event-thumbnail (click)="handleThumbnailClick(event.name)"  [event]="event"></event-thumbnail>
         
+                </div>
+            </div>
     </div>
     `
 })
-export class EventsListComponent {
-    event1 = {
-        id: 1,
-        name: 'Angular Connect',
-        date: '9/26/2036',
-        time: '10:00 am',
-        price: 599.99,
-        imageUrl: '/app/assets/images/angularconnect-shield.png',
-        location: {
-            address: '1057 DT',
-            city: 'London',
-            country: 'England'
-        }
-    }
+export class EventsListComponent implements OnInit {
+    events:any[]
 
+    constructor(private eventService: EventService,
+        private toastr: ToastrService){
+    }
     
+    ngOnInit(){
+        
+        this.events = this.eventService.getEvents()
+    }
+    handleThumbnailClick(eventName){
+        this.toastr.success(eventName)
+    }
 };
